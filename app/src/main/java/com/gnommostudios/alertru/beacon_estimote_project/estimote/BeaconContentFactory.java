db76dbb582eed611ab9d6ee83b747de4410605c8
ChristianDAM <christian_sellent@hotmail.com>
2018-05-22 14:54:12 +0200

    App predefinida

---
+++ app/src/main/java/com/gnommostudios/alertru/beacon_estimote_project/estimote/BeaconContentFactory.java
@@ -0,0 +1,10 @@
+package com.gnommostudios.alertru.beacon_estimote_project.estimote;
+
+public interface BeaconContentFactory {
+
+    void getContent(BeaconID beaconID, Callback callback);
+
+    interface Callback {
+        void onContentReady(Object content);
+    }
+}
