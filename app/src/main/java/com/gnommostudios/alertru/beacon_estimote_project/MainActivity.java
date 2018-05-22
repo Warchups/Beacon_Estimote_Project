570c5ce40a9ad5f9667c3ab19ea82a4f49d73c50
ChristianDAM <christian_sellent@hotmail.com>
2018-05-22 16:01:33 +0200

    Prueba mal

--- app/src/main/java/com/gnommostudios/alertru/beacon_estimote_project/MainActivity.java
+++ app/src/main/java/com/gnommostudios/alertru/beacon_estimote_project/MainActivity.java
@@ -1,96 +1,79 @@
 package com.gnommostudios.alertru.beacon_estimote_project;
 
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.estimote.coresdk.cloud.model.Color;
-import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
-import com.gnommostudios.alertru.beacon_estimote_project.estimote.BeaconID;
-import com.gnommostudios.alertru.beacon_estimote_project.estimote.EstimoteCloudBeaconDetails;
-import com.gnommostudios.alertru.beacon_estimote_project.estimote.EstimoteCloudBeaconDetailsFactory;
-import com.gnommostudios.alertru.beacon_estimote_project.estimote.ProximityContentManager;
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collection;
 
-//
-// Running into any issues? Drop us an email to: contact@estimote.com
-//
-
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-
-    private static final Map<Color, Integer> BACKGROUND_COLORS = new HashMap<>();
-
-    static {
-        BACKGROUND_COLORS.put(Color.ICY_MARSHMALLOW, android.graphics.Color.rgb(109, 170, 199));
-        BACKGROUND_COLORS.put(Color.BLUEBERRY_PIE, android.graphics.Color.rgb(98, 84, 158));
-        BACKGROUND_COLORS.put(Color.MINT_COCKTAIL, android.graphics.Color.rgb(155, 186, 160));
-    }
-
-    private static final int BACKGROUND_COLOR_NEUTRAL = android.graphics.Color.rgb(160, 169, 172);
-
-    private ProximityContentManager proximityContentManager;
+public class MainActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
+    protected static final String TAG = "MainActivity";
+    private BeaconManager mBeaconManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        proximityContentManager = new ProximityContentManager(this,
-                Arrays.asList(
-                        // TODO: replace with UUIDs, majors and minors of your own beacons
-                        new BeaconID("00000074-6162-6c65-7465-63682d617070", 1111, 2222),
-                        new BeaconID("00000000-0000-0000-0000-000000000001", 3333, 4444)),
-                new EstimoteCloudBeaconDetailsFactory());
-        proximityContentManager.setListener(new ProximityContentManager.Listener() {
-            @Override
-            public void onContentChanged(Object content) {
-                String text;
-                Integer backgroundColor;
-                if (content != null) {
-                    EstimoteCloudBeaconDetails beaconDetails = (EstimoteCloudBeaconDetails) content;
-                    text = "You're in " + beaconDetails.getBeaconName() + "'s range!";
-                    backgroundColor = BACKGROUND_COLORS.get(beaconDetails.getBeaconColor());
-                } else {
-                    text = "No beacons in range.";
-                    backgroundColor = null;
-                }
-                ((TextView) findViewById(R.id.textView)).setText(text);
-                findViewById(R.id.relativeLayout).setBackgroundColor(
-                        backgroundColor != null ? backgroundColor : BACKGROUND_COLOR_NEUTRAL);
-            }
-        });
-    }
-
-    @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
+        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        // Detect the main Eddystone-UID frame:
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+        // Detect the telemetry Eddystone-TLM frame:
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT));
+        mBeaconManager.bind(this);
+    }
 
-        if (!SystemRequirementsChecker.checkWithDefaultDialogs(this)) {
-            Log.e(TAG, "Can't scan for beacons, some pre-conditions were not met");
-            Log.e(TAG, "Read more about what's required at: http://estimote.github.io/Android-SDK/JavaDocs/com/estimote/sdk/SystemRequirementsChecker.html");
-            Log.e(TAG, "If this is fixable, you should see a popup on the app's screen right now, asking to enable what's necessary");
-        } else {
-            Log.d(TAG, "Starting ProximityContentManager content updates");
-            proximityContentManager.startContentUpdates();
+    public void onBeaconServiceConnect() {
+        Region region = new Region("all-beacons-region", null, null, null);
+        try {
+            mBeaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        mBeaconManager.addRangeNotifier(this);
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        for (Beacon beacon: beacons) {
+            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
+                // This is a Eddystone-UID frame
+                Identifier namespaceId = beacon.getId1();
+                Identifier instanceId = beacon.getId2();
+                Log.d(TAG, "I see a beacon transmitting namespace id: "+namespaceId+
+                        " and instance id: "+instanceId+
+                        " approximately "+beacon.getDistance()+" meters away.");
+
+                // Do we have telemetry data?
+                if (beacon.getExtraDataFields().size() > 0) {
+                    long telemetryVersion = beacon.getExtraDataFields().get(0);
+                    long batteryMilliVolts = beacon.getExtraDataFields().get(1);
+                    long pduCount = beacon.getExtraDataFields().get(3);
+                    long uptime = beacon.getExtraDataFields().get(4);
+
+                    Log.d(TAG, "The above beacon is sending telemetry version "+telemetryVersion+
+                            ", has been up for : "+uptime+" seconds"+
+                            ", has a battery level of "+batteryMilliVolts+" mV"+
+                            ", and has transmitted "+pduCount+" advertisements.");
+
+                }
+            }
         }
     }
 
     @Override
-    protected void onPause() {
+    public void onPause() {
         super.onPause();
-        Log.d(TAG, "Stopping ProximityContentManager content updates");
-        proximityContentManager.stopContentUpdates();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        proximityContentManager.destroy();
+        mBeaconManager.unbind(this);
     }
 }
