4a261ba1b16ea1ad9a90c5cc126e6fbeec1886d1
ChristianDAM <christian_sellent@hotmail.com>
2018-05-23 11:27:47 +0200

    Prueba mal

--- app/src/main/java/com/gnommostudios/alertru/beacon_estimote_project/MainActivity.java
+++ app/src/main/java/com/gnommostudios/alertru/beacon_estimote_project/MainActivity.java
@@ -1,79 +1,164 @@
 package com.gnommostudios.alertru.beacon_estimote_project;
 
+import android.Manifest;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseSettings;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
 import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
-public class MainActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
+public class MainActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier, View.OnClickListener {
+
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
     protected static final String TAG = "MainActivity";
     private BeaconManager mBeaconManager;
+    private Button button;
 
     @Override
-    public void onResume() {
-        super.onResume();
-        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
-        // Detect the main Eddystone-UID frame:
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+        Log.i(TAG, "*************");
+
+        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "No llego aqui");
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("This app needs location access");
+            builder.setMessage("Please grant location access so this app can detect beacons.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+
+                }
+            });
+            builder.show();
+        }
+
+
+        mBeaconManager = BeaconManager.getInstanceForApplication(this);
+        // En este ejemplo vamos a usar el protocolo Eddystone, así que tenemos que definirlo aquí
         mBeaconManager.getBeaconParsers().add(new BeaconParser().
                 setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
-        // Detect the telemetry Eddystone-TLM frame:
-        mBeaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT));
+        // Bindea esta actividad al BeaconService
         mBeaconManager.bind(this);
+
+        /*final Beacon beacon = new Beacon.Builder()
+                .setId1("00000074-6162-6c65-7465-63682d617070")
+                .setId2("4369")
+                .setId3("8738")
+                .setManufacturer(0x0000) // Radius Networks.  Change this for other beacon layouts
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[]{0l})) // Remove this for beacon layouts without d: fields
+                .build();
+
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        beaconTransmitter.startAdvertising(beacon, new AdvertiseCallback() {
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.i(TAG, "Advertisement start failed with code: "+errorCode);
+            }
+
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                Log.i(TAG, "Advertisement start succeeded.");
+                Log.i("Name beacon", beacon.getBluetoothName()+"");
+            }
+        });*/
+
+
+
     }
 
     public void onBeaconServiceConnect() {
-        Region region = new Region("all-beacons-region", null, null, null);
+        ArrayList<Identifier> identifiers = new ArrayList<>();
+
+        identifiers.add(null);
+
+        Region region = new Region("AllBeaconsRegion", identifiers);
+
         try {
             mBeaconManager.startRangingBeaconsInRegion(region);
         } catch (RemoteException e) {
             e.printStackTrace();
         }
+
         mBeaconManager.addRangeNotifier(this);
     }
 
     @Override
     public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-        for (Beacon beacon: beacons) {
-            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
-                // This is a Eddystone-UID frame
-                Identifier namespaceId = beacon.getId1();
-                Identifier instanceId = beacon.getId2();
-                Log.d(TAG, "I see a beacon transmitting namespace id: "+namespaceId+
-                        " and instance id: "+instanceId+
-                        " approximately "+beacon.getDistance()+" meters away.");
+        if (beacons.size() > 0) {
+            Log.i(TAG, "El primer beacon detectado se encuentra a una distancia de " + beacons.iterator().next().getDistance() + " metros.");
+        } else {
+            //Log.i(TAG, "*************");
+        }
+    }
 
-                // Do we have telemetry data?
-                if (beacon.getExtraDataFields().size() > 0) {
-                    long telemetryVersion = beacon.getExtraDataFields().get(0);
-                    long batteryMilliVolts = beacon.getExtraDataFields().get(1);
-                    long pduCount = beacon.getExtraDataFields().get(3);
-                    long uptime = beacon.getExtraDataFields().get(4);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.i(TAG, "Aqui llego?");
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                Log.i(TAG, "Llego aqui?");
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "coarse location permission granted");
+                } else {
+                    Log.i(TAG, "Y aqui llego?");
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
 
-                    Log.d(TAG, "The above beacon is sending telemetry version "+telemetryVersion+
-                            ", has been up for : "+uptime+" seconds"+
-                            ", has a battery level of "+batteryMilliVolts+" mV"+
-                            ", and has transmitted "+pduCount+" advertisements.");
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
 
+                    });
+                    builder.show();
                 }
+                return;
             }
         }
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        mBeaconManager.unbind(this);
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+
+                break;
+        }
     }
 }
