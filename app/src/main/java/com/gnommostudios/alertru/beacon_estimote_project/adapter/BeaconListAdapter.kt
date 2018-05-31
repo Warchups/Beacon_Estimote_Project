d886d90ed3e3f50e75ccd5f5841cbb58f9d2518f
ChristianDAM <christian_sellent@hotmail.com>
2018-05-31 12:34:59 +0200

    .

--- app/src/main/java/com/gnommostudios/alertru/beacon_estimote_project/adapter/BeaconListAdapter.kt
+++ app/src/main/java/com/gnommostudios/alertru/beacon_estimote_project/adapter/BeaconListAdapter.kt
@@ -16,7 +16,7 @@ class BeaconListAdapter(private val context: Activity, private val elements: Mut
     private val decimalFormat = DecimalFormat("0.00")
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var convertView: View?
+        val convertView: View?
         val inflater = context.layoutInflater
         convertView = inflater.inflate(R.layout.element_list, null)
         val element = elements[position]
